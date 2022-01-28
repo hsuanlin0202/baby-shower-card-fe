d7a6dd741819b4cc1808a5a20a0198b391ece270
sherin.lin <sherin.lin@car-plus.com.tw>
2022-01-29 00:38:32 +0800

    vendor order add / edit form done

---
+++ components/pages/vendor/order/FormGroup.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import clsx from "clsx";
+
+type FormGroupProps = {
+  title: string;
+  children: ReactNode;
+  icon?: ReactNode;
+  titleSpace?: string;
+  contentSpace?: string;
+};
+export const FormGroup = ({
+  title,
+  children,
+  icon,
+  titleSpace = "min-w-40",
+  contentSpace = "w-56",
+}: FormGroupProps): JSX.Element => {
+  return (
+    <div className="flex items-center mx-8 my-4">
+      {!!title && (
+        <div className={clsx("flex items-center", titleSpace)}>
+          {icon && icon}
+          <span>{title}</span>
+        </div>
+      )}
+      <div className={contentSpace}>{children}</div>
+    </div>
+  );
+};
