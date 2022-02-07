3d11c9b4891d5835c7cdb2506507d632ea4d8506
kimimberlyyen <work223305@gmail.com>
2022-02-07 23:49:10 +0800

    feat: 🎸 first commit
    not yet, pedding

---
+++ pages/baby/index.tsx
@@ -0,0 +1,25 @@
+export default function () {
+  return (
+    <div className="card">
+      <div className="photo">
+        <img src="" alt="logo" />
+        <img src="" alt="baby" />
+      </div>
+      <div className="text">
+        <div className="babyName"></div>
+        <div className="story"></div>
+        <div className="parents">
+          <div className="papa"></div>
+          <div className="mama"></div>
+        </div>
+        <div className="date"></div>
+      </div>
+      <input type="button" />
+      <div className="icon">
+        <div className="item like"></div>
+        <div className="item comment"></div>
+        <div className="item shares"></div>
+      </div>
+    </div>
+  );
+}
