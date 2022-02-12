2b143000d46ee096d7a57bd2f9711c1918e311e3
sherin.lin <sherin.lin@car-plus.com.tw>
2022-02-12 19:35:17 +0800

    baby [id] inline css -> css in js

--- components/pages/baby/BabyCardPage.tsx
+++ components/pages/baby/BabyCardPage.tsx
@@ -12,89 +12,107 @@ export const BabyCardPage = ({ card }: Props): JSX.Element => {
   if (!card) return <></>;
 
   return (
-    <div
-      className="w-full bg-center bg-contain flex flex-col items-center"
-      style={{
-        color: card.template.textColor,
-        backgroundImage: `url('${card.template.background}')`,
-      }}
-    >
-      <div className="w-full md:w-phone min-h-screen pt-4 pb-10 flex flex-col items-center space-y-4">
-        <div>
-          {/* 這兩項暫時在api端寫死，因為資料庫還沒新增欄位 */}
+    <>
+      <style jsx>
+        {`
+          .card-background-image {
+            background-image: url("${card.template.background}");
+            background-position: center;
+            background-size: contain;
+          }
 
-          {card.template.logo && (
-            <img className="h-14" src={card.template.logo} alt="logo" />
-          )}
+          .glass {
+            background-color: rgba(255, 255, 255, 0.1);
+            backdrop-filter: blur(2px);
+          }
 
-          {card.template.partner && (
-            <h2 className="text-center baby-main-font">
-              {card.template.partner}
-            </h2>
-          )}
-        </div>
+          .text-color {
+            color: ${card.template.textColor};
+          }
 
-        <img className="w-5/6" src={card.photo} alt="babyPhoto330x330" />
+          .colored-background {
+            background-color: ${card.template.textColor};
+            color: ${getContrastColorByLightness(card.template.textColor)};
+          }
+        `}
+      </style>
 
-        <section className="flex flex-col items-center space-y-2 baby-main-font">
-          <h1 className="text-2xl font-semibold">
-            {`${card.babyName}，滿月囉！`}
-          </h1>
+      <div className="card-background-image text-color w-full flex justify-center overflow-hidden">
+        <div className="w-screen h-screen glass" />
 
-          <p
-            className="max-w-80p text-base leading-relaxed text-center"
-            dangerouslySetInnerHTML={{
-              __html: card.description.replaceAll("\n", "<br/>"),
-            }}
-          />
+        <div className="card-background-image shadow-lg fixed top-0 w-full md:w-phone h-screen m-0 pt-4 flex flex-col items-center justify-between space-y-4 overflow-y-scroll">
+          <div>
+            {/* 這兩項暫時在api端寫死，因為資料庫還沒新增欄位 */}
 
-          <p className="text-2xl font-semibold">
-            {`爸爸 ${card.fatherName}  &  媽媽 ${card.motherName}`}
-          </p>
-        </section>
+            {card.template.logo && (
+              <img className="h-12" src={card.template.logo} alt="logo" />
+            )}
 
-        <div className="text-3xl font-bold baby-date-font">
-          {DateStringFormat(card.babyBirthday)}
-        </div>
+            {card.template.partner && (
+              <h2 className="text-center baby-main-font">
+                {card.template.partner}
+              </h2>
+            )}
+          </div>
 
-        <button
-          type="button"
-          className="px-10 py-2 text-sm rounded-md"
-          style={{
-            backgroundColor: card.template.textColor,
-            color: getContrastColorByLightness(card.template.textColor),
-          }}
-          onClick={() => {}}
-        >
-          <span>保存回憶</span>
-        </button>
+          <img className="w-5/6" src={card.photo} alt="babyPhoto330x330" />
 
-        <div className="w-full flex justify-evenly">
-          <SocialButton
-            icon={<FavoriteBorderIcon />}
-            title="Like"
+          <section className="flex flex-col items-center space-y-2 baby-main-font">
+            <h1 className="text-2xl font-semibold">
+              {`${card.babyName}，滿月囉！`}
+            </h1>
+
+            <p
+              className="max-w-80p text-base leading-relaxed text-center"
+              dangerouslySetInnerHTML={{
+                __html: card.description.replaceAll("\n", "<br/>"),
+              }}
+            />
+
+            <p className="text-2xl font-semibold">
+              {`爸爸 ${card.fatherName}  &  媽媽 ${card.motherName}`}
+            </p>
+          </section>
+
+          <div className="text-3xl font-bold baby-date-font">
+            {DateStringFormat(card.babyBirthday)}
+          </div>
+
+          <button
+            type="button"
+            className="px-10 py-2 text-sm rounded-md colored-background"
             onClick={() => {}}
-          />
-          <SocialButton
-            icon={<ChatBubbleOutlineIcon />}
-            title="Comment"
-            onClick={() => {}}
-          />
-          <SocialButton
-            icon={<ShareOutlinedIcon />}
-            title="Share"
-            onClick={() => {}}
-          />
-        </div>
+          >
+            <span>保存回憶</span>
+          </button>
 
-        <a href="#">
-          <p className="mb-4 text-xs underline">彌月禮盒滿意度調查</p>
-        </a>
+          <div className="w-full flex justify-evenly">
+            <SocialButton
+              icon={<FavoriteBorderIcon />}
+              title="Like"
+              onClick={() => {}}
+            />
+            <SocialButton
+              icon={<ChatBubbleOutlineIcon />}
+              title="Comment"
+              onClick={() => {}}
+            />
+            <SocialButton
+              icon={<ShareOutlinedIcon />}
+              title="Share"
+              onClick={() => {}}
+            />
+          </div>
+
+          <a href="#">
+            <p className=" text-xs underline">彌月禮盒滿意度調查</p>
+          </a>
+
+          <footer className="w-full h-10 leading-10 colored-background md:bg-transparent text-xs text-center">
+            <p>Created by Joy Baby Card</p>
+          </footer>
+        </div>
       </div>
-
-      <footer className="w-full -mt-10 h-10 leading-10 bg-brown-200 text-xs text-brown-500 text-center">
-        <p>Created by Joy Baby Card</p>
-      </footer>
-    </div>
+    </>
   );
 };
